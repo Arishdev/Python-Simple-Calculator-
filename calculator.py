468d929bcbfdba35a61843ea01672a6c586aabf6
Arishdev <136901855+Arishdev@users.noreply.github.com>
2023-06-18 13:25:03 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,27 @@
+# https://youtube.com/@ultimategaming1075 on Youtube
 
+i = int(input("First: "))
+
+e = int(input("Second: "))
+
+o = input("Operator (+,-,*,/): ")
+
+if o == "+":
+
+	print ("Answer is ", i + e)elif o == "-":
+
+	print ("Answer is ", i - e)
+
+elif o == "*":
+
+	print ("Answer is ", i * e)
+
+elif o == "/":
+
+	print ("Answer is ", i / e)
+
+else:
+
+	print("ok")
+
+print ("Thank you for using me ツ")
